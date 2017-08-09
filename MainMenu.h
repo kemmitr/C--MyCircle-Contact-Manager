a9b896fb054a5c29f717a4f67e10352705b50986
Russell Kemmit <russell.kemmit@smail.rasmussen.edu>
2017-08-09 18:13:15 -0500

    Create MainMenu.h

---
+++ MainMenu.h
@@ -0,0 +1,24 @@
+/////////////////////////////////////////
+//FileName: MainMenu.h
+//FileType: C++ header File
+//Author: Russell Kemmit
+//Created On: 7/10/17 10:02 AM
+//Last Modified: 8/7/2017
+//Description: this is the MainMenu header file
+/////////////////////////////////////////
+#include <iostream>
+#include<map>
+using namespace std;
+
+#ifndef MAINMENU_H_
+#define MAINMENU_H_
+
+class MainMenu {
+public:
+	MainMenu();
+	void Password();
+	void Menu();
+
+};
+
+#endif /* MAINMENU_H_ */
