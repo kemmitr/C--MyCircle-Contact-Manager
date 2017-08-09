872078e1f91588a610a03a3d012255267cbfd664
Russell Kemmit <russell.kemmit@smail.rasmussen.edu>
2017-08-09 18:12:54 -0500

    Create MainMenu.cpp

---
+++ MainMenu.cpp
@@ -0,0 +1,54 @@
+/////////////////////////////////////////
+//FileName: MainMenu.cpp
+//FileType: C++ source File
+//Author: Russell Kemmit
+//Created On: 7/10/17 10:02 AM
+//Last Modified: 8/7/2017
+//Description: this is the MainMenu class
+/////////////////////////////////////////
+#include "MainMenu.h"
+#include "AddContact.h"
+MainMenu::MainMenu() {
+}
+void MainMenu::Password() {
+	cout << "Please Enter Your Password. " << endl;
+	string pass;
+	cin >> pass;
+	if (pass != "Delta") {
+		cout<<"Password incorrect please try again."<<endl;
+		Password();
+	}
+}
+//below I created a switch for the customer to use the menu.
+void MainMenu::Menu() {
+	AddContact con;
+
+	cout << "!!MyCircle Contact!!" << endl;
+	cout << endl;
+
+	int selection;
+	do {
+		cout << "***************************" << endl;
+		cout << "1. Add New Contact         " << endl;
+		cout << "2. View Contacts      " << endl;
+		cout << "3. Search For Contacts      " << endl;
+		cout << "4. Press 0 to Shut Down Program " << endl;
+		cout << "***************************" << endl;
+		cin>> selection;
+		switch(selection){
+		case 0:
+			cout<<"Shutting Down..."<<endl;
+			break;
+		case 1:
+			con.ADD();
+			break;
+			default:
+		case 2:
+			con.view();
+			break;
+		case 3:
+			con.find();
+			break;
+		}
+	} while (selection != 0);
+}
