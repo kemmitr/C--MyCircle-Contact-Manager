d2c11696cceff227756897317c6fca020c6129f5
Russell Kemmit <russell.kemmit@smail.rasmussen.edu>
2018-10-16 11:47:07 -0500

    Update MainMenu.h

--- MainMenu.h
+++ MainMenu.h
@@ -1,11 +1,3 @@
-/////////////////////////////////////////
-//FileName: MainMenu.h
-//FileType: C++ header File
-//Author: Russell Kemmit
-//Created On: 7/10/17 10:02 AM
-//Last Modified: 8/7/2017
-//Description: this is the MainMenu header file
-/////////////////////////////////////////
 #include <iostream>
 #include<map>
 using namespace std;
